25ab5878474f017422f51494185c993888e7ac81
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-07-27 20:32:23 +0600

    task 4 solved

---
+++ task-3.js
@@ -0,0 +1,19 @@
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 4
+
+const PrimeNumber = 119;
+const theRemainder = 5;
+const theOutPut = PrimeNumber % theRemainder;
+console.log(theOutPut);
